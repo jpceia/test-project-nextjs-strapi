d996b5c0fb5454ff52e6b9b67361f92e5bb9bc74
João Ceia <joao.p.ceia@gmail.com>
2022-06-25 06:15:56 +0100

    removed unused import

--- client/common/auth/index.tsx
+++ client/common/auth/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import { PropsWithChildren, User } from "../types";
 
 interface AuthContextType {
