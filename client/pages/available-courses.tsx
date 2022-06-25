4b67c9d25a7fb10caa705e7c241b84b04030ee4d
João Ceia <joao.p.ceia@gmail.com>
2022-06-25 06:28:10 +0100

    removed hardcoded api_key from available-courses.tsx

--- client/pages/available-courses.tsx
+++ client/pages/available-courses.tsx
@@ -5,8 +5,6 @@ import PrivateLayout from "../common/components/PrivateLayout";
 import { ICoursesResponse, ICourse } from "../common/types";
 import styles from "../styles/AvailableCourses.module.css"
 
-const api_key = "d432de8d8264489a4de74bb45daddb17b84a11c28283881d75bbda2a35b046596483c8592b7d0dfeb05c1986af2e1757dc037ea8591ce95cfefa254c5447f17e363024dc42c8675205bff8f909c74e48d8a3d5810485e1f5f26e05ee494b2cc039baa9cd7b4705240fea392faaf20b37a3720f0ebd3af6f4be0eee9762e77031"
-
 
 const AvailableCourses: NextPage = () => {
   const [courses, setCourses] = useState<ICoursesResponse | undefined>(undefined);
