7656b607cc487509237895048103d961e6226d89
João Ceia <joao.p.ceia@gmail.com>
2022-06-27 09:30:50 +0100

    fixed css in home

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next'
 
+
 const Home: NextPage = () => {
   return (
     <div>
-      <h4>Bem vindo 😀 Seleccione a opção que pretende na barra de navegacção</h4>
+      <h4 style={{textAlign: "center"}}>Bem vind@ 😀 Seleccione a opção que pretende na barra de navegacção</h4>
     </div>
   );
 }
