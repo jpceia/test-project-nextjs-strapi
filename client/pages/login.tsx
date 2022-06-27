d98e57c9db1f7763fbd091b0a1afe7c0ec22084e
João Ceia <joao.p.ceia@gmail.com>
2022-06-27 05:56:17 +0100

    moved Login and RegisterBox to corresponding pages

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,11 +1,51 @@
 import { NextPage } from "next";
-import LoginBox from "../common/components/LoginBox";
+import { SyntheticEvent, useState } from "react";
 import PublicGuard from "../common/components/PublicGuard";
+import { useGlobalCtx } from "../common/context";
+
 
 const Login: NextPage = () => {
+
+  const [ email, setEmail ] = useState<string>('');
+  const [ password, setPassword ] = useState<string>('');
+  const { loginUser, error } = useGlobalCtx();
+
+  const onSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+    loginUser(email, password);
+  }
+
   return (
     <PublicGuard>
-      <LoginBox />
+      <div>
+        { error && <p style={{color: "red"}}>{error}</p> }
+        <form onSubmit={onSubmit}>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="submit"
+              value="Entrar"
+            />
+          </div>
+        </form>
+      </div>
     </PublicGuard>
   );
 }
--- client/pages/register.tsx
+++ client/pages/register.tsx
@@ -1,13 +1,64 @@
 import { NextPage } from "next";
 import PublicGuard from "../common/components/PublicGuard";
-import RegisterBox from "../common/components/RegisterBox";
+import { SyntheticEvent, useState } from "react";
+import { useGlobalCtx } from "../common/context";
 
-const Login: NextPage = () => {
+
+const Register: NextPage = () => {
+
+  const [ email, setEmail ] = useState('');
+  const [ name, setName ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const { registerUser, error } = useGlobalCtx();
+
+
+  const onSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+    registerUser(name, email, password);
+  }
+
   return (
     <PublicGuard>
-      <RegisterBox />
+      <div>
+        { error && <p style={{color: "red"}}>{error}</p> }
+        <form onSubmit={onSubmit}>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="name">Nome</label>
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="submit"
+              value="Entrar"
+            />
+          </div>
+        </form>
+      </div>
     </PublicGuard>
   );
 }
 
-export default Login;
+export default Register;
