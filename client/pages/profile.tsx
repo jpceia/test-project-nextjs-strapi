eedea22d0feb25c5b93fbed1bfe719480e8add28
João Ceia <joao.p.ceia@gmail.com>
2022-06-27 08:53:39 +0100

    improved profile page

--- client/pages/profile.tsx
+++ client/pages/profile.tsx
@@ -5,17 +5,20 @@ import PrivateGuard from "../common/components/PrivateGuard";
 const Profile: NextPage = () => {
 
   const { user } = useGlobalCtx();
+  const { username, email, createdAt } = user!;
+  const dateStr = new Date(createdAt!).toLocaleDateString();
   
   return (
     <PrivateGuard>
+      <h1>Perfil</h1>
       <p>
-        <strong>Nome:</strong>{user?.username}
+        <strong>Nome</strong><br />{username}
       </p>
       <p>
-        <strong>E-mail:</strong>{user?.email}
+        <strong>E-mail</strong><br />{email}
       </p>
       <p>
-        <strong>Data de registo:</strong>{user?.createdAt}
+        <strong>Data de registo</strong><br />{dateStr}
       </p>
     </PrivateGuard>
   );
