3874408bfff735946951b38e5fbb14688b5fba6c
João Ceia <joao.p.ceia@gmail.com>
2022-06-28 04:38:20 +0100

    added local-storage file with useLocalStorage hook

---
+++ client/common/state/local-storage.ts
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export const useLocalStorage = <T,>(keyName: string, defaultValue: T): [value: T, setValue: (val: T) => void] => {
+
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try
+    {
+      const value = window.localStorage.getItem(keyName);
+      if (value) {
+        return JSON.parse(value);
+      }
+      else {
+        window.localStorage.setItem(keyName, JSON.stringify(defaultValue));
+        return defaultValue;
+      }
+    }
+    catch (err) {
+      return defaultValue;
+    }
+  });
+
+  const setValue = (newValue: T) => {
+    try {
+      window.localStorage.setItem(keyName, JSON.stringify(newValue));
+    }
+    catch (err) {}
+    setStoredValue(newValue);
+  };
+
+  return [storedValue, setValue];
+};
+
+export default useLocalStorage;
