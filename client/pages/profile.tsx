7090a5b45b3d6f68a1186cfa7af12939cf45aec0
João Ceia <joao.p.ceia@gmail.com>
2022-06-28 04:57:32 +0100

    protecting Profile page agains null user

--- client/pages/profile.tsx
+++ client/pages/profile.tsx
@@ -5,21 +5,25 @@ import PrivateGuard from "../common/components/PrivateGuard";
 const Profile: NextPage = () => {
 
   const { user } = useGlobalCtx();
-  const { username, email, createdAt } = user!;
-  const dateStr = new Date(createdAt!).toLocaleDateString();
   
   return (
     <PrivateGuard>
       <h1>Perfil</h1>
-      <p>
-        <strong>Nome</strong><br />{username}
-      </p>
-      <p>
-        <strong>E-mail</strong><br />{email}
-      </p>
-      <p>
-        <strong>Data de registo</strong><br />{dateStr}
-      </p>
+      {
+        user && (
+          <div>
+            <p>
+              <strong>Nome</strong><br />{user.username}
+            </p>
+            <p>
+              <strong>E-mail</strong><br />{user.email}
+            </p>
+            <p>
+              <strong>Data de registo</strong><br />{new Date(user.createdAt!).toLocaleDateString()}
+            </p>
+          </div>
+        )
+      }
     </PrivateGuard>
   );
 }
